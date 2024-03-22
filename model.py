740169a7bb354c4c80d1eba29b6ee2bab53369e8
Rahul Jain <rahuldsce@gmail.com>
2024-03-22 18:53:39 +0530

    Update model.py

--- model.py
+++ model.py
@@ -37,6 +37,55 @@ class Mnist(nn.Module):
         x = self.fc2(x)  # Apply second fully connected layer (output layer)
         return F.log_softmax(x, dim=1)  # Apply log softmax function to get output probabilities
 
+class Cifar10_BN(nn.Module):
+  def conv_block (self, in_channels, out_channels, kernel_size, padding = 1) :
+        return nn.Sequential(
+              nn.Conv2d (in_channels = in_channels, out_channels = out_channels, kernel_size = kernel_size, padding = padding, bias = False),
+              nn.ReLU(),
+              nn.BatchNorm2d(out_channels),
+              nn.Dropout(0.1))
+
+  def trans_block (self, in_channels, out_channels):
+    return nn.Sequential(
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size = 1, padding = 0, bias = False))
+
+  def out_block(self, in_channels, kernel_size = 1):
+    return nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=10, kernel_size = kernel_size, padding = 0, bias = False))
+
+  def __init__(self, opts=[]):
+        super(Cifar10_BN, self).__init__()
+        self.conv1 = self.conv_block(3, 16, 3) #32
+        self.conv2 = self.conv_block(16, 32, 3) #32
+        self.trans1 = self.trans_block(32, 16) #32
+        self.conv3 = self.conv_block(16, 16, 3) #16
+        self.conv4 = self.conv_block(16, 32, 3) #16
+        self.conv5 = self.conv_block(32, 32, 3) #16
+        self.trans2 = self.trans_block(32, 16) #16
+        self.conv6 = self.conv_block(16, 16, 3) #8
+        self.conv7 = self.conv_block(16, 32, 3) #8
+        self.conv8 = self.conv_block(32, 32, 3) #8
+        self.gap = nn.Sequential(nn.AvgPool2d(kernel_size=8))
+        self.out = self.out_block(32, 1)
+        self.pool = nn.MaxPool2d(2, 2)
+
+
+  def forward(self, x):
+      x = self.conv1(x)
+      x = self.conv2(x)
+      x = self.trans1(x)
+      x = self.conv3(x)
+      x = self.conv4(x)
+      x = self.conv5(x)
+      x = self.trans2(x)
+      x = self.conv6(x)
+      x = self.conv7(x)
+      x = self.conv8(x)
+      x = self.gap(x)
+      x = self.out(x)
+      x = x.view(-1, 10)
+      return F.log_softmax(x, dim=-1)
+
 # Group Normalization
 class Cifar10_GN(nn.Module):
   def conv_block (self, in_channels, out_channels, kernel_size, padding = 1) :
