df8b4df2c73c855ea37f9277c151c98fac9ba874
Rahul Jain <rahuldsce@gmail.com>
2024-03-22 16:03:18 +0530

    Add mint and cifar10

--- dataset.py
+++ dataset.py
@@ -1,1 +1,24 @@
+# dataset.py
 
+import numpy as np
+
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+from torch.utils.data import Dataset
+from torchvision import datasets
+
+from . import transforms
+from . import datasetmgr
+
+def mnist():
+  train_transforms, test_transforms = transforms.mnist_transforms()
+  train = datasets.MNIST('./data', train=True, download=True, transform=train_transforms)
+  test = datasets.MNIST('./data', train=False, download=True, transform=test_transforms)
+  return train, test
+
+def cifar10():
+  train_transforms, test_transforms = transforms.cifar10_transforms()
+  train = datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transforms)
+  test = datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transforms)
+  return train, test
