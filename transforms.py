ae62a5eeb7824efe452cc884d731b686612beb83
Rahul Jain <rahuldsce@gmail.com>
2024-03-29 21:41:18 +0530

    Update transforms.py

--- transforms.py
+++ transforms.py
@@ -47,12 +47,12 @@ def cifar10_transforms_alb():
      CoarseDropout(max_holes=1, max_height=16, max_width=16,  # Corrected parameters
                   min_holes=1, min_height=16, min_width=16,   # Corrected parameters
                   fill_value=(0.5, 0.5, 0.5), mask_fill_value=None),
-     transforms.ToTensor(),
+     transforms.ToTensorV2(),
      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
    ])
   
   transform_test = transforms.Compose([
-     transforms.ToTensor(),
+     transforms.ToTensorV2(),
      transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
    ])
   
